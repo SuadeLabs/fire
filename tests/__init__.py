fe4939dcb21222d4eeaae1ba87cc03ca8e6672f0
murat <murat@suade.org>
2020-06-08 18:49:42 +0100

    Updates enum test

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 
@@ -12,3 +13,52 @@ SCHEMA_NAMES = [f.split(".json")[0] for f in SCHEMA_FILES]
 _, _, filenames = next(os.walk(DOCS_DIR), (None, None, []))
 DOC_FILES = [f for f in filenames if f.endswith(".md")]
 DOC_NAMES = [f.split(".md")[0] for f in DOC_FILES]
+
+
+def schema_properties(schema_name):
+    """
+    Returns the properties for a schema
+    """
+    schema = fire_load(schema_name)
+    if schema_name == "common.json":
+        properties = schema.keys()
+    else:
+        properties = schema["properties"].keys()
+
+    return properties
+
+
+def schema_enum_registry(schema_name):
+    """
+    Returns a dictionary of "property name" : [enum values]
+    """
+    enums = {}
+
+    schema = fire_load(schema_name)
+    if schema_name == "common.json":
+        properties = schema
+    else:
+        properties = schema["properties"]
+
+    for prop, obj in properties.items():
+        if "$ref" in obj:
+            prop = obj["$ref"].split("/")[-1]
+            obj = fire_load("common.json")[prop]
+        if "enum" in obj:
+            enums[prop] = obj["enum"]
+
+    return enums
+
+
+def all_properties():
+    properties = set()
+    for schema_name in SCHEMA_FILES:
+        {properties.add(prop) for prop in schema_properties(schema_name)}
+
+    return properties
+
+
+def fire_load(schema_name):
+    with open(os.path.join(SCHEMAS_DIR, schema_name)) as json_schema:
+        schema = json.load(json_schema)
+    return schema
--- tests/test_schemas.py
+++ tests/test_schemas.py
@@ -1,11 +1,14 @@
 import json
 import os
+import re
 import unittest
 from . import (
     DOC_NAMES,
     SCHEMAS_DIR,
     SCHEMA_FILES,
     SCHEMA_NAMES,
+    all_properties,
+    schema_enum_registry,
 )
 
 
@@ -20,18 +23,79 @@ class TestSchemas(unittest.TestCase):
             with open(os.path.join(SCHEMAS_DIR, schema_name)) as json_schema:
                 self.assertTrue(json.load(json_schema))
 
+    def test_enum_registry(self):
+        for schema in SCHEMA_FILES:
+            enums = schema_enum_registry(schema)
+            if schema in ["batch.json", "guarantor.json", "issuer.json"]:
+                self.assertFalse(enums)
+            else:
+                self.assertTrue(enums)
+
+    def test_enums(self):
+        """
+        Enums should be snake_case (mostly)
+        Enums should be less than 24 characters
+        """
+        def snake_error(schema, enum, value):
+            return "{} > {} > {} isn't snake_case!".format(
+                schema, enum, value)
+
+        def len_error(schema, enum, value, length):
+            return "{} > {} > {} longer than 22 chars! ({})".format(
+                schema, enum, value, length)
+
+        exceptions = [
+            "country_code",  # ISO 3166
+            "currency_code",  # ISO 4217
+            "base_currency_code",  # ISO 4217
+            "quote_currency_code",  # ISO 4217
+            "underlying_index_tenor",  # day convention
+            "base_rate",  # Bloomberg tickers,
+        ]
+        long_names = [
+            "independent_collateral_amount",
+            "interest_repayment_frequency",
+            "buy_to_let_house_purchase",
+            "buy_to_let_further_advance",
+            "cancelled_payout_agreed",
+            "firm_operating_expenses",
+        ]
+
+        snake_pattern = re.compile("[a-z][a-z0-9]*(_[a-z0-9]*)*")
+        for schema in SCHEMA_FILES:
+            enums = schema_enum_registry(schema)
+            for enum, values in enums.items():
+                match = re.match(snake_pattern, enum)
+                self.assertIsNotNone(match, snake_error(schema, enum, ""))
+                self.assertEqual(
+                    0, match.start(), snake_error(schema, enum, ""))
+                self.assertEqual(
+                    len(enum), match.end(), snake_error(schema, enum, ""))
+
+                if enum not in long_names:
+                    self.assertLessEqual(
+                        len(enum), 22,
+                        len_error(schema, enum, "", len(enum))
+                    )
+                if enum in exceptions:
+                    continue
+                for v in values:
+                    match = re.match(snake_pattern, v)
+                    self.assertIsNotNone(match, snake_error(schema, enum, v))
+                    self.assertEqual(
+                        0, match.start(), snake_error(schema, enum, v))
+                    self.assertEqual(
+                        len(v), match.end(), snake_error(schema, enum, v))
+
+                    if v not in long_names:
+                        self.assertLessEqual(
+                            len(v), 22,
+                            len_error(schema, enum, v, len(v))
+                        )
+
     def test_property_has_docs(self):
         """TODO: add documentation for adjustment schema"""
-        properties = set()
-        for schema_name in SCHEMA_FILES:
-            with open(os.path.join(SCHEMAS_DIR, schema_name)) as json_schema:
-                schema = json.load(json_schema)
-                if schema_name == "common.json":
-                    props = schema.keys()
-                else:
-                    props = schema["properties"].keys()
-
-                {properties.add(p) for p in props}
+        properties = all_properties()
 
         self.assertTrue(properties)
 
