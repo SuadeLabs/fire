0333972f601db734b3fa584cc82fa4ef976e781d
Jonathan <jonathan@suade.org>
2020-06-30 09:54:12 +0100

    Add example of eq future with index composition

--- tests/test_schemas.py
+++ tests/test_schemas.py
@@ -32,7 +32,9 @@ class TestSchemas(unittest.TestCase):
     def test_enum_registry(self):
         for schema in SCHEMA_FILES:
             enums = schema_enum_registry(schema)
-            if schema in ["batch.json", "guarantor.json", "issuer.json"]:
+            if schema in [
+                "batch.json", "guarantor.json", "issuer.json", "index.json"
+            ]:
                 self.assertFalse(enums)
             else:
                 self.assertTrue(enums)
