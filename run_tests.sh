73fcb34ca024dc4e901ee9656f072acbb92cb9ee
murat <murat@suade.org>
2021-08-11 16:05:10 +0100

    Revert "Fix CI tests"
    This reverts commit 2e0f943f1c681ba932071135074ac051c2a8321b.

--- run_tests.sh
+++ run_tests.sh
@@ -6,7 +6,10 @@ echo "===> Flake8 Running..."
 flake8 . --exclude "*venv*"
 echo "===> Flake8 Finished"
 
-echo "===> Python Tests Running..."
-pytest -v
-echo "===> Python Tests Finished"
+echo "===> Python2 Tests Running..."
+python2 -m pytest -v
+echo "===> Python2 Tests Finished"
 
+echo "===> Python3 Tests Running..."
+python3 -m pytest -v
+echo "===> Python3 Tests Finished"
