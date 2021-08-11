ef1ed9468cca4d9ec01c5c1e74a9a6e77c98f119
murat <murat@suade.org>
2021-08-11 15:57:07 +0100

    Update to run python2 and 3 tests

--- run_tests.sh
+++ run_tests.sh
@@ -6,6 +6,10 @@ echo "===> Flake8 Running..."
 flake8 . --exclude "*venv*"
 echo "===> Flake8 Finished"
 
-echo "===> PyTest Running..."
-pytest -v
-echo "===> PyTest Finished"
+echo "===> Python2 Tests Running..."
+python2 -m pytest -v
+echo "===> Python2 Tests Finished"
+
+echo "===> Python3 Tests Running..."
+python3 -m pytest -v
+echo "===> Python3 Tests Finished"
